fc2f7935bbea8499d5ea30e2e4dd6ed1febcff07
MatoElias <mato951129@gmail.com>
2025-12-01 19:36:37 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,36 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+let count = 0;
+
+function addOne() {
+    count += 1
+    homeScore.textContent = count
+}
+
+function addTwo() {
+    count += 2
+    homeScore.textContent = count
+}
+
+function addThree() {
+    count += 3
+    homeScore.textContent = count
+}
+
+countGuest = 0
+
+function addOneGuest() {
+    countGuest += 1
+    guestScore.textContent = countGuest
+}
+
+function addTwoGuest() {
+    countGuest += 2
+    guestScore.textContent = countGuest
+}
+
+function addThreeGuest() {
+    countGuest += 3
+    guestScore.textContent = countGuest
+}
